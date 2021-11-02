1db50fb1c37413869f8fc0ee78b9eeffacf3746c
Swasti-123 <86404753+Swasti-123@users.noreply.github.com>
2021-11-02 17:54:51 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 menu_list_array = ["Veg Margherita Pizza", "Panner Tikka Pizza", "Cheese-Stuffed Veg Pizza", "Margherita", "Extra Cheese Vegetarian Pizza", "Gourmet Pizza"];
 
 function getmenu(){
-var htmldata;
+var htmldata="";
 menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+ menu_list_array[i] + '<br>'
@@ -10,12 +10,12 @@ menu_list_array.sort();
 }
 
 function add_item(){
-var htmldata;
+var htmldata="";
 var item=document.getElementById("add_item").value;
         menu_list_array.sort();
         htmldata=""
         for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ <img class="img-responsive" src="pizza.png"></img>+ menu_list_array[i]+'<br>';
+            htmldata=htmldata+ "<img class=img-responsive src='pizza.png'> <span>"+ menu_list_array[i]+"</span>"+'<br>';
         }
          document.getElementById("display_addedmenu").innerHTML = htmldata;		
       }
